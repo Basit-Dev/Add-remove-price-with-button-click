dc089eecfe09f4aa65da77567565017d724161f5
Abdul Basit <63122554+Basit-Dev@users.noreply.github.com>
2021-07-02 18:23:01 +0100

    Moved all the logic of functions and state to component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,12 @@
 import "./styles.css";
 import React from "react";
-import checkedList from "./isCheckedList";
 import DisplayLists from "./Components/DisplayLists";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-      isChecked: checkedList
-    };
-  }
-
-  componentDidMount() {
-    this.setState(() => {
-      return {
-        isChecked: checkedList
-      };
-    });
-  }
-
-  increment = (price) => {
-    this.setState((prevState) => {
-      return {
-        count: prevState.count + price
-      };
-    });
-  };
-
-  decrement = (e, price) => {
-    // Disable the button if the count is 0
-    if (this.state.count === 0) {
-      e.target = "disabled";
-    } else {
-      e.target.style.backgroundColor = "lightBlue";
-      this.setState((prevState) => {
-        return {
-          count: prevState.count - price
-        };
-      });
-    }
-  };
-
   render() {
     return (
-      <div className="container">
-        <DisplayLists
-          list={this.state.isChecked}
-          increment={this.increment}
-          decrement={this.decrement}
-        />
-        <div className="total">Total: {this.state.count}</div>
+      <div>
+        <DisplayLists />
       </div>
     );
   }
--- src/Components/DisplayLists.js
+++ src/Components/DisplayLists.js
@@ -1,22 +1,68 @@
 import React from "react";
+import checkedList from "../isCheckedList";
+import "./display-list.css";
 
-const DisplayLists = (props) => {
-  return props.list.map((data) => {
+class DisplayLists extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      isChecked: checkedList
+    };
+  }
+
+  componentDidMount() {
+    this.setState(() => {
+      return {
+        isChecked: checkedList
+      };
+    });
+  }
+
+  increment = (price) => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + price
+      };
+    });
+  };
+
+  decrement = (e, price) => {
+    // Disable the button if the count is 0
+    if (this.state.count === 0) {
+      e.target = "disabled";
+    } else {
+      e.target.style.backgroundColor = "lightBlue";
+      this.setState((prevState) => {
+        return {
+          count: prevState.count - price
+        };
+      });
+    }
+  };
+  render() {
     return (
-      <div key={data.id} className="label">
-        <h1>ID: {data.id}</h1>
-        <p>Name: {data.name}</p>
-        <p>IsChecked: {data.checked.toString()}</p>
-        <p>£{data.price}</p>
-        <button onClick={() => props.increment(data.price)}>Add</button>
-        <button
-          style={{ marginLeft: "10px" }}
-          onClick={(e) => props.decrement(e, data.price)}
-        >
-          Remove
-        </button>
+      <div className="container">
+        {this.state.isChecked.map((data) => {
+          return (
+            <div key={data.id} className="label-box">
+              <h1>ID: {data.id}</h1>
+              <p>Name: {data.name}</p>
+              <p>IsChecked: {data.checked.toString()}</p>
+              <p>£{data.price}</p>
+              <button onClick={() => this.increment(data.price)}>Add</button>
+              <button
+                style={{ marginLeft: "10px" }}
+                onClick={(e) => this.decrement(e, data.price)}
+              >
+                Remove
+              </button>
+            </div>
+          );
+        })}
+        <div className="total">Total: {this.state.count}</div>
       </div>
     );
-  });
-};
+  }
+}
 export default DisplayLists;
